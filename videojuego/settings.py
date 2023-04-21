27c691caac3408307922be961058784b6cf6087c
Diego Figueroa Peart <a01660987@tec.mx>
2023-04-21 08:24:28 -0600

    csrf fix

--- videojuego/settings.py
+++ videojuego/settings.py
@@ -28,10 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['matemagicas.azurewebsites.net',
                  '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://matemagicas.azurewebsites.net'
-    '127.0.0.1'
-]
+CSRF_TRUSTED_ORIGINS = ['https://matemagicas.azurewebsites.net',
+                        '127.0.0.1']
 
 
 # Application definition
